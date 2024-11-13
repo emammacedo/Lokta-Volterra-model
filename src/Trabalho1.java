fa22fe8b3f8783fee1c405fabdff8d5a4d40782b
Ema Macedo <139995966+emammacedo@users.noreply.github.com>
2024-11-13 11:06:34 +0000

    Update Trabalho1.java

--- src/Trabalho1.java
+++ src/Trabalho1.java
@@ -1,23 +1,16 @@
-/* ExercÌcio de avaliaÁ„o 1
- * 
- * Ana Catarina Pais Pereira - 2019244449
- * Ema Margarida da Silva Macedo - 2019233271
- * 
- Modelo Predador - Presa */
-
 import java.util.Scanner;
 
 public class Trabalho1
 {
 	/*
 	 * t -> tempo; x -> presas; alfa -> taxa crescimento presas; beta -> taxa de decrescimo presas; y -> predadores;
-	 * delta -> taxa crescimento predadores; gama -> taxa de decrescimo predadores; h -> intervalo de discretizaÁ„o
+	 * delta -> taxa crescimento predadores; gama -> taxa de decrescimo predadores; h -> intervalo de discretiza√ß√£o
 	 * temporal
 	 */
 
 	//// Recolha dos valores dados pelo utilizador
 
-	// N⁄MEROS DECIMAIS --> ,
+	// N√öMEROS DECIMAIS --> ,
 
 	public static void main(String[] args)
 	{
@@ -36,13 +29,13 @@ public class Trabalho1
 		// valor t
 		do
 		{
-			System.out.println("Insira o tempo de simulaÁ„o:");
-			if (sc.hasNextDouble()) // leitura e verificaÁ„o de que um valor numÈrico pode ser lido
+			System.out.println("Insira o tempo de simula√ß√£o:");
+			if (sc.hasNextDouble()) // leitura e verifica√ß√£o de que um valor num√©rico pode ser lido
 			{
 				double t_max = sc.nextDouble(); // aguardar por um double
 				while (t_max <= 0)
 				{
-					System.out.println("Insira um n˙mero superior a 0");
+					System.out.println("Insira um n√∫mero superior a 0");
 					t_max = sc.nextDouble();
 				}
 				valt = true;
@@ -51,14 +44,14 @@ public class Trabalho1
 				// valor h
 				do
 				{
-					System.out.println("Intervalo de discretizaÁ„o temporal:");
+					System.out.println("Intervalo de discretiza√ß√£o temporal:");
 
 					if (sc.hasNextDouble())
 					{
 						double h = sc.nextDouble();
 						while (h >= 1 || h <= 0)
 						{
-							System.out.println("Insira um n˙mero entre 0 e 1");
+							System.out.println("Insira um n√∫mero entre 0 e 1");
 							h = sc.nextDouble();
 						}
 						valh = true;
@@ -67,17 +60,17 @@ public class Trabalho1
 						// valor x_i
 						do
 						{
-							System.out.println("Insira a populaÁ„o inicial de presas:");
+							System.out.println("Insira a popula√ß√£o inicial de presas:");
 							if (sc.hasNextInt())
 							{
 								double x_i = sc.nextInt();
 								/*
-								 * aceita apenas um n˙mero inteiro, mas a variavÈl criada È um double para mais tarde
-								 * ser utilizado nos c·lculos
+								 * aceita apenas um n√∫mero inteiro, mas a variav√©l criada √© um double para mais tarde
+								 * ser utilizado nos c√°lculos
 								 */
 								while (x_i <= 0)
 								{
-									System.out.println("Insira um n˙mero superior a 0");
+									System.out.println("Insira um n√∫mero superior a 0");
 									x_i = sc.nextInt();
 								}
 								valx = true;
@@ -86,13 +79,13 @@ public class Trabalho1
 								// valor alfa
 								do
 								{
-									System.out.println("Insira a taxa de crescimento da populaÁ„o de presas:");
+									System.out.println("Insira a taxa de crescimento da popula√ß√£o de presas:");
 									if (sc.hasNextDouble())
 									{
 										double alfa = sc.nextDouble();
 										while (alfa <= 0 || alfa > 1)
 										{
-											System.out.println("Insira um n˙mero entre 0 e 1");
+											System.out.println("Insira um n√∫mero entre 0 e 1");
 											alfa = sc.nextDouble();
 										}
 										valalfa = true;
@@ -101,13 +94,13 @@ public class Trabalho1
 										// valor beta
 										do
 										{
-											System.out.println("Insira a taxa de decrÈscimo da populaÁ„o de presas:");
+											System.out.println("Insira a taxa de decr√©scimo da popula√ß√£o de presas:");
 											if (sc.hasNextDouble())
 											{
 												double beta = sc.nextDouble();
 												while (beta <= 0 || beta > 1)
 												{
-													System.out.println("Insira um n˙mero entre 0 e 1");
+													System.out.println("Insira um n√∫mero entre 0 e 1");
 													beta = sc.nextDouble();
 												}
 												valbeta = true;
@@ -116,14 +109,14 @@ public class Trabalho1
 												// valor y_i
 												do
 												{
-													System.out.println("Insira a populaÁ„o inicial de predadores:");
+													System.out.println("Insira a popula√ß√£o inicial de predadores:");
 													if (sc.hasNextInt())
 													{
 														double y_i = sc.nextInt(); 
 														
 														while (y_i <= 0)
 														{
-															System.out.println("Insira um n˙mero supeior a 0");
+															System.out.println("Insira um n√∫mero supeior a 0");
 															y_i = sc.nextInt();
 														}
 														valy = true;
@@ -133,13 +126,13 @@ public class Trabalho1
 														do
 														{
 															System.out.println(
-																	"Insira a taxa de crescimento da populaÁ„o de predadores:");
+																	"Insira a taxa de crescimento da popula√ß√£o de predadores:");
 															if (sc.hasNextDouble())
 															{
 																double delta = sc.nextDouble();
 																while (delta <= 0 || delta > 1)
 																{
-																	System.out.println("Insira um n˙mero entre 0 e 1");
+																	System.out.println("Insira um n√∫mero entre 0 e 1");
 																	delta = sc.nextDouble();
 																}
 																valdelta = true;
@@ -149,20 +142,20 @@ public class Trabalho1
 																do
 																{
 																	System.out.println(
-																			"Insira a taxa de decrÈscimo da populaÁ„o de predadores:");
+																			"Insira a taxa de decr√©scimo da popula√ß√£o de predadores:");
 																	if (sc.hasNextDouble())
 																	{
 																		double gama = sc.nextDouble();
 																		while (gama <= 0 || gama > 1)
 																		{
 																			System.out.println(
-																					"Insira um n˙mero entre 0 e 1");
+																					"Insira um n√∫mero entre 0 e 1");
 																			gama = sc.nextDouble();
 																		}
 																		valgama = true;
 																		sc.nextLine();
 
-																		//// ExecuÁ„o
+																		//// Execu√ß√£o
 																		
 																		// vetor c/ valores de x (presas)
 																		double[] val_x = new double[(int) (t_max / h) + 1];
@@ -170,7 +163,7 @@ public class Trabalho1
 																		// vetor c/ valores de y (predadores)
 																		double[] val_y = new double[(int) (t_max / h) + 1];
 																		
-																		//adiÁ„o dos valores iniciais pedidos ao utilizador
+																		//adi√ß√£o dos valores iniciais pedidos ao utilizador
 																		val_x[0] = x_i;
 																		val_y[0] = y_i;
 
@@ -182,14 +175,14 @@ public class Trabalho1
 																			r = func.rk4(x_i, y_i, alfa, beta, delta,
 																					gama, h);
 
-																			// adiÁ„o dos valores aos vetores
+																			// adi√ß√£o dos valores aos vetores
 																			val_x[n] = r[0];
 																			val_y[n] = r[1];
 
-																			n = n + 1; // atualizaÁ„o do Ìndice
+																			n = n + 1; // atualiza√ß√£o do √≠ndice
 
-																			// atualizaÁ„o dos valores de x e y para a
-																			// prÛxima integraÁ„o
+																			// atualiza√ß√£o dos valores de x e y para a
+																			// pr√≥xima integra√ß√£o
 																			x_i = r[0];
 																			y_i = r[1];
 																		}
@@ -204,7 +197,7 @@ public class Trabalho1
 																		System.out.println(
 																				"---------------------------------");
 																																			
-																		//// DeterminaÁ„o do valor m·ximo atingido
+																		//// Determina√ß√£o do valor m√°ximo atingido
 																		double max_x = 0;
 																		double max_y = 0;
 																		for (int j = 0; j < val_x.length; j++)
@@ -219,7 +212,7 @@ public class Trabalho1
 																			}
 																		}														
 
-																		//// Impress„o do valor m·ximo atingido
+																		//// Impress√£o do valor m√°ximo atingido
 																		double v_max = 0; 
 																		if (max_y > max_x)
 																			v_max = max_y;
@@ -228,17 +221,17 @@ public class Trabalho1
 
 																		System.out.println(v_max);
 
-																		//// Gr·fico
+																		//// Gr√°fico
 
-																		// intervalo correspondente a cada traÁo eixo y
-																		// n˙mero de linhas do gr·fico = 35
+																		// intervalo correspondente a cada tra√ßo eixo y
+																		// n√∫mero de linhas do gr√°fico = 35
 																		double comp_inter = v_max / 35; 
 
-																		// intervalo correspondente a cada traÁo eixo x
-																		// n˙mero de colunas do gr·fico = 124
+																		// intervalo correspondente a cada tra√ßo eixo x
+																		// n√∫mero de colunas do gr√°fico = 124
 																		double comp_tempo = t_max / 124; 
 
-																		// vetores c/ valores de x e y mÈdios
+																		// vetores c/ valores de x e y m√©dios
 																		double[] val_x_new = new double[124];
 																		double[] val_y_new = new double[124];
 
@@ -246,8 +239,8 @@ public class Trabalho1
 																		for (double i = 0; i < t_max; i = i
 																				+ comp_tempo)
 																		{
-																			/*indice do 1∫ valor de x/y a ter em conta no c·lculo de 
-																			 * x/y mÈdio para o intervalo [i;i+comp_t]
+																			/*indice do 1¬∫ valor de x/y a ter em conta no c√°lculo de 
+																			 * x/y m√©dio para o intervalo [i;i+comp_t]
 																			*/
 																			int nn = (int) (i / h); // assume o
 																									// inteiro por
@@ -266,7 +259,7 @@ public class Trabalho1
 																				count += 1;
 																			}
 
-																			//valor x/y mÈdio para o intervalo de tempo [i;i+comp_tempo]
+																			//valor x/y m√©dio para o intervalo de tempo [i;i+comp_tempo]
 																			val_x_new[novo] = sum_x / count;
 																			val_y_new[novo] = sum_y / count;
 
@@ -274,13 +267,13 @@ public class Trabalho1
 
 																		}
 
-																		//matriz gr·fico
+																		//matriz gr√°fico
 																		char[][] graf = new char[35][124]; 
 
-																		// Impress„o do gr·fico
+																		// Impress√£o do gr√°fico
 																		int d = 0;
-																		double lim_sup = v_max; // limite superior do 1∫
-																								// valor mÈdio = maximo
+																		double lim_sup = v_max; // limite superior do 1¬∫
+																								// valor m√©dio = maximo
 
 																		System.out.println('^');
 																		for (int i = 0; i <= 35 - 1; i++)
@@ -289,7 +282,7 @@ public class Trabalho1
 																									// coloca um |
 
 																			d += 1; // intervalo correspondente a cada
-																					// valor mÈdio È "somado" de linha
+																					// valor m√©dio √© "somado" de linha
 																					// para linha
 
 																			for (int j = 0; j <= 124 - 1; j++)
@@ -301,14 +294,14 @@ public class Trabalho1
 																				
 																				for (int m = 0; m < val_x_new.length; m++)
 																				{
-																					//se estiver dentro do intervalor em quest„o
+																					//se estiver dentro do intervalor em quest√£o
 																					if (val_y_new[m] < lim_sup
 																							& val_y_new[m] >= lim_inf)
 																					{
 																						graf[i][m] = '*'; // predador
 																					}
 																					
-																					//se estiver dentro do intervalor em quest„o
+																					//se estiver dentro do intervalor em quest√£o
 																					if (val_x_new[m] <= lim_sup
 																							& val_x_new[m] >= lim_inf) 
 																					{
@@ -353,7 +346,7 @@ public class Trabalho1
 																	else
 																	{
 																		System.out.println(
-																				"O n˙mero inserido n„o È v·lido.");
+																				"O n√∫mero inserido n√£o √© v√°lido.");
 																		sc.nextLine();
 																	}
 																}
@@ -361,7 +354,7 @@ public class Trabalho1
 															}
 															else
 															{
-																System.out.println("O n˙mero inserido n„o È v·lido.");
+																System.out.println("O n√∫mero inserido n√£o √© v√°lido.");
 																sc.nextLine();
 															}
 														}
@@ -369,7 +362,7 @@ public class Trabalho1
 													}
 													else
 													{
-														System.out.println("O n˙mero inserido n„o È v·lido.");
+														System.out.println("O n√∫mero inserido n√£o √© v√°lido.");
 														sc.nextLine();
 													}
 												}
@@ -377,7 +370,7 @@ public class Trabalho1
 											}
 											else
 											{
-												System.out.println("O n˙mero inserido n„o È v·lido.");
+												System.out.println("O n√∫mero inserido n√£o √© v√°lido.");
 												sc.nextLine();
 											}
 										}
@@ -385,7 +378,7 @@ public class Trabalho1
 									}
 									else
 									{
-										System.out.println("O n˙mero inserido n„o È v·lido.");
+										System.out.println("O n√∫mero inserido n√£o √© v√°lido.");
 										sc.nextLine();
 									}
 								}
@@ -393,7 +386,7 @@ public class Trabalho1
 							}
 							else
 							{
-								System.out.println("O n˙mero inserido n„o È v·lido.");
+								System.out.println("O n√∫mero inserido n√£o √© v√°lido.");
 								sc.nextLine();
 							}
 						}
@@ -401,7 +394,7 @@ public class Trabalho1
 					}
 					else
 					{
-						System.out.println("O intervalo inserido n„o È v·lido.");
+						System.out.println("O intervalo inserido n√£o √© v√°lido.");
 						sc.nextLine();
 					}
 				}
@@ -409,7 +402,7 @@ public class Trabalho1
 			}
 			else
 			{
-				System.out.println("O tempo inserido n„o È v·lido.");
+				System.out.println("O tempo inserido n√£o √© v√°lido.");
 				sc.nextLine();
 			}
 		}
